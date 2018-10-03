5e1d1e886a1a7c1a2957ba11fcd6921b31e09369
NaumanMalik <nomi_malik01@yahoo.com>
2018-10-03 22:56:10 +0400

    initial commit

---
+++ server.js
@@ -0,0 +1,42 @@
+
+const express = require('express');
+const hbs  = require('hbs');
+var app = express();
+
+hbs.registerPartials(__dirname+ '/views/partials');
+app.set('view engine', hbs);
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+})
+hbs.registerHelper('screamIt', (text) => {
+return text.toUpperCase();
+});
+
+app.use(express.static(__dirname + '/public' ));
+
+
+
+app.get('/', (req, res) => {
+    // res.send('<h1>hello express</h1>');
+    // res.send({
+    //     name:"Azhar",
+    //     likes:['1','2']
+    // })
+    res.render('Home.hbs', {
+        title: 'Home',
+        pageTitle: 'Welcome',
+
+    })
+});
+
+app.get('/about', (req, res) => {
+    // res.send('<h1>hello express</h1>');
+res.render('About.hbs',{
+    pageTitle: 'About Page',
+
+});
+  
+});
+
+app.listen(3000);
+
